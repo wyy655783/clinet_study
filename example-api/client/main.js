bf04bcebfb265e595f7ae5ec307fd4835916622a
wyy <wyy655783@163.com>
2025-08-24 23:29:35 +0800

    js学习完成

--- example-api/client/main.js
+++ example-api/client/main.js
@@ -1,4 +1,8 @@
 import express from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware'   
 const app=express()
+app.use('/api',createProxyMiddleware({
+    target:'http://localhost:8080',changeOrigin:true
+}))
 app.use(express.static('./'))
 app.listen(7070)
