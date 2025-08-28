36b5e090a605768ca1659c4bd768617715b3b77f
wyy <wyy655783@163.com>
2025-08-28 23:33:56 +0800

    vue2  4days

--- example-api/vue_client/src/views/ExpDataView.vue
+++ example-api/vue_client/src/views/ExpDataView.vue
@@ -14,8 +14,32 @@ export default {
         baseURL: 'http://localhost:8080',
         withCredentials: true,
       });
-      const resp = await _axios.post('/getUs', {});
-      console.log(resp);
+      try {
+        const resp = await _axios.get('/getUs');
+        console.log(resp);
+      } catch (error) {
+        const { status } = error.response;
+        console.log(error.response);
+        switch (status) {
+          case 400:
+            console.error('请求参数错误');
+            break;
+          case 401:
+            console.error('未授权，请登录');
+            break;
+          case 403:
+            console.error('没有权限');
+            break;
+          case 404:
+            console.error('资源不存在');
+            break;
+          case 500:
+            console.error('服务器错误');
+            break;
+          default:
+            console.error('未知错误');
+        }
+      }
     },
   },
 };
